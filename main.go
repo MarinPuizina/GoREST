00de31719ac7f3bf2e4c3e105117ec988460d427
MarinPuizina <marinpuizina@hotmail.com>
2021-01-30 03:01:41 +0100

    Add simple REST API

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-	r := mux.NewRounter()
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Home page")
 }
