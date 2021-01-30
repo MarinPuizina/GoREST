ac5909b475f6110ad9e19ced02598c26488b4425
MarinPuizina <marinpuizina@hotmail.com>
2021-01-30 03:50:40 +0100

    Add endpoint that accepts string params,logs them and prints them in response

--- main.go
+++ main.go
@@ -15,8 +15,23 @@ func main() {
 		io.WriteString(w, "Hello from handler func 2")
 	}
 
+	userHandler := func(w http.ResponseWriter, r *http.Request) {
+		keys, ok := r.URL.Query()["key"]
+
+		if !ok || len(keys[0]) < 1 {
+			log.Println("Url param key is missing")
+			return
+		}
+
+		key := keys[0]
+
+		io.WriteString(w, "Url param key is="+string(key))
+		log.Println("Url param key is=" + string(key))
+	}
+
 	http.HandleFunc("/", h1)
 	http.HandleFunc("/endpoint", h2)
+	http.HandleFunc("/user", userHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
