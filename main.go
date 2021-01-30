c0dbfdb1d5808e9bacb40c669df16456e9f2baeb
MarinPuizina <marinpuizina@hotmail.com>
2021-01-30 03:27:49 +0100

    Add making two handlers for two endpoints

--- main.go
+++ main.go
@@ -1,16 +1,37 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", homePage)
+	h1 := func(w http.ResponseWriter, _ *http.Request) {
+		io.WriteString(w, "Hello from handler func 1")
+	}
+
+	h2 := func(w http.ResponseWriter, _ *http.Request) {
+		io.WriteString(w, "Hello from handler func 2")
+	}
+
+	http.HandleFunc("/", h1)
+	http.HandleFunc("/endpoint", h2)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	/*s := &server{}
+	http.HandleFunc("/", s)
+	log.Fatal(http.ListenAndServe(":8080", nil))*/
 }
 
+/*
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Home page")
 }
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r * http.Reqeust) {
+	w.Header().set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte('{"message": "Hello Lasma"}'))
+}*/
